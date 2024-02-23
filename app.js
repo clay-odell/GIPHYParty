eaebf2a9944d9e9090276fe07da1f03058d84de8
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-23 16:01:05 -0600

    submitHandle asynch function added

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("Let's get this party started!");
+
+const form = document.getElementById("searchBar");
+form.addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  const searchTerm = document.querySelector("#searchTerm").value;
+  try {
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        q: searchTerm,
+        api_key: "9JhZMlmqUVgnhe7d9iw9bEHobFedOfhL",
+      },
+    });
+    console.log(response.data);
+    
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
